88ccd58d27681a21e573df7032dd03ee7f951900
Alvaro1370 <alvarosteel45@gmail.com>
2024-12-23 10:34:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
@@ -42,15 +42,15 @@ global.scan = "https://nikkapair.onrender.com/pair";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "" // session id here
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWUp3d0dYcG9TTmdjeDJEYy96MmltUG43RE0xMzlDTDBPTER1NWZuaXYwST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVVB2djR4REtEV0hYWithSnFMZWRuK0t2UHRmei85citkMWNPV3NVV0drWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBR0UrMjJkNUFHZ212K3pUQmxsRCsrM3R5a21vZnFqTXJYWmVmSmpIRDNJPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFU0FoMVg0UHZaWHIydHk1bjBVL1lFUnpacGdXZkNuVFJkSkJOYUpjNXc0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im9MWldyalQ3OHYyNnpVaDA3WnladWtNK3l6Q3JWd0x3Tk1KN2pCM2IrWDQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlJyeDM4bjJhdGtpaWdLdmRpY0pZWSsrVk1zQU5BMC9mdnpLTzVMWUorQnM9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR1BIdUdlWlZ5QUlScTZWdU1DQ1luVVdVTytjSlNpU0NLZ2JtSTRYQjkyOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZXlkc1ByczJ0WGliREhPcVRXa2VXcGM5eEVaVmtZR1Nhc1Jhbk1hbVl6WT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InZFVDkyT25VWFhWQTZUc2g5UFBjdElsS3ZqM0JDSGZVZkR5UXI5L2hTbVVyaVQvdjlOaHZhNmFYS1YvMkNjbGNGOGRjekNCUlg5blI5TlFiblZWcWh3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTcyLCJhZHZTZWNyZXRLZXkiOiJoU08vQjRhMFYxeHpmQ3NyWjM0U3ArSngvQS9tYncwN0hlOGFvM1IvV0lVPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJ2TllMd2ZMSlNRS0xiekxsOUtndWh3IiwicGhvbmVJZCI6IjNkMTIyY2FiLTE4ZTktNDRlZi05NjVhLWI4ZWNiYzljZjFkOSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJyTG9McmVDVnZqRjhsc2pYc1A3VmUvbEh2NFk9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMUFGL3FDMkRSS09Pd2pIYXEvejgwZEJXRys4PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjhEUk5KRkZTIiwibWUiOnsiaWQiOiIyMzQ4MTA5MjQzNjE4OjYwQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNNMnkxODhFRVB2YXBMc0dHQ1lnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJXTDJSNTlFcUIxOUhTeFhRenp2UkVtNWtyTVRRb1VIZ2EyT2pDUE5YcWpnPSIsImFjY291bnRTaWduYXR1cmUiOiJJbE11cGVJbzY1Y0JWenFQQisxNWZjTU9XUEdLeGdMUCthc2NSS3lDTk5XNFJhYmpTb2tNVmtGOTZSQ0xSTXlEUHhMTmVOK3FYR2FGNFM3emNRQmtEdz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiNVhGb0lYSGRrYzZ4ZEVNUy9wK2l4Wm9JSXpUS2lIc1VPMy94c0Z0U0k3RjNNU1A2clBzbHJmdzdJdkhEVEdqNkJSRXBMaWNnYTFRN1N3TlJMbTJlaUE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzQ4MTA5MjQzNjE4OjYwQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlZpOWtlZlJLZ2RmUjBzVjBNODcwUkp1Wkt6RTBLRkI0R3Rqb3dqelY2bzQifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzQ5NDYxODV9" // session id here
 module.exports = {
   menu: process.env.MENU || "2",
-  HANDLERS: process.env.PREFIX || ",",
+  HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`PATRON-MD™`",
-  author: process.env.PACK_AUTHER || "KING",
-  packname: process.env.PACK_NAME || "PATRON",
+  author: process.env.PACK_AUTHER || "𝕯𝖆𝖛𝖎𝖉",
+  packname: process.env.PACK_NAME || "𝕵𝖊 𝖛𝖔𝖎𝖘 𝖘𝖙𝖎𝖈𝖐𝖊𝖗 𝖕𝖆𝖈𝖐",
   botname: process.env.BOT_NAME || "PATRON-MD",
   ownername: process.env.OWNER_NAME || "PATRON",
   errorChat: process.env.ERROR_CHAT || "",
